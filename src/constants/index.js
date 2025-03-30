6c3bf14b08f8d1c7c22e652204a1dacbd535d895
Saimiksha <saimiksha.agarwal@cumminscollege.in>
2025-03-30 17:32:53 +0530

    delete

--- src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export const navItems = [
-    {label: "About", href: "src/components/AboutUs.html"},
-    {label: "Live! Fish Detection System", href: "src/components/Satellite1.html"},
-    {label: "Contact Us", href: "src/components/ContactUs.html"},
-];
-   
