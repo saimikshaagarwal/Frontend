a0cb42b0f0cd5d7b389edc770007957ebb25a0df
Saimiksha <saimiksha.agarwal@cumminscollege.in>
2025-03-30 19:32:22 +0530

    ch

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const navItems = [
-    {label: "About", href: "/AboutUs.html"},
-    {label: "Live! Fish Detection System", href: "/Satellite1.html"},
-    {label: "Contact Us", href: "/ContactUs.html"},
+    {label: "About", href: "src/components/AboutUs.html"},
+    {label: "Live! Fish Detection System", href: "src/components/Satellite1.html"},
+    {label: "Contact Us", href: "src/components/ContactUs.html"},
 ];
    
