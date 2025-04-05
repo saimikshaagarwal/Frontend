0f220be2b3ccd0d4a35905b7900313538740d8a7
Saimiksha <saimiksha.agarwal@cumminscollege.in>
2025-04-05 19:36:20 +0530

    change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,9 +25,8 @@ function Navbar() {
 
     return (
         <div className="relative w-full overflow-x-hidden">
-            <nav className='sticky top-0 z-50 backdrop-blur-lg border-b border-neutral-700/80 w-full'>
-                <div className='container px-4 mx-auto flex justify-between items-center w-full'>
-                    {/* Logo Section */}
+            <nav className='sticky top-0 z-50 backdrop-blur-lg border-b border-neutral-700/80 w-full' onClick={toggleNavbar}>
+                <div className='container px-4 mx-auto relative lg:text-sm flex justify-between items-center w-full'>
                     <div className='flex items-center flex-shrink-0'>
                         <h2 className='text-2xl font-bold flex items-center gap-2 text-[#003366]'>
                             <img src="/logo.jpeg" className="w-12 h-12" alt="Logo" />
@@ -36,44 +35,43 @@ function Navbar() {
                             </Link>
                         </h2>
                     </div>
-
-                    {/* Desktop Navigation */}
-                    <div className='hidden lg:flex items-center space-x-6'>
-                        <ul className='flex space-x-8 ml-14'>
+                    <div className='flex space-x-6 items-center'>
+                        <div className='hidden lg:flex ml-14 space-x-12'>
                             {navItems.map((item, index) => (
                                 <li key={index} className='list-none'>
                                     <a href={item.href} className='text-[#003366] hover:text-[#00509e]'>{item.label}</a>
                                 </li>
                             ))}
-                        </ul>
-
-                        {isLoggedIn ? (
-                            <>
-                                <span className="text-sm text-[#003366]">Welcome, {user.email}</span>
-                                <button
-                                    onClick={handleLogout}
-                                    className='py-2 px-3 border rounded-md text-[#003366] hover:bg-[#00509e]'>
-                                    Logout
-                                </button>
-                            </>
-                        ) : (
-                            <>
-                                <a href='/login' className='py-2 px-3 border rounded-md text-[#003366] hover:text-[#00509e]'>
-                                    Sign In
-                                </a>
-                                <a href='/signup' className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md text-white'>
-                                    Sign Up
-                                </a>
-                            </>
-                        )}
-
-                        {/* Language Widget */}
-                        <div className="elfsight-app-a55d9b4c-4790-4775-9e0d-66d770209238 w-8 h-8"></div>
+                        </div>
+                        <div className='hidden lg:flex justify-center space-x-6 items-center'>
+                            {isLoggedIn ? (
+                                <>
+                                    <span className="text-sm text-[#003366]">Welcome, {user.email}</span>
+                                    <button
+                                        onClick={handleLogout}
+                                        className='py-2 px-3 border rounded-md text-[#003366] hover:bg-[#00509e]'>
+                                        Logout
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <a href='/login' className='py-2 px-3 border rounded-md text-[#003366] hover:text-[#00509e]'>
+                                        Sign In
+                                    </a>
+                                    <a href='/signup' className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md'>
+                                        Sign Up
+                                    </a>
+                                </>
+                            )}
+                        </div>
+                        {/* Language Widget at Right Corner */}
+                        <div className='hidden lg:flex items-center space-x-2'>
+                            <script src="https://static.elfsight.com/platform/platform.js" async></script>
+                            <div className="elfsight-app-a55d9b4c-4790-4775-9e0d-66d770209238 w-8 h-8" data-elfsight-app-lazy></div>
+                        </div>
                     </div>
                 </div>
             </nav>
-
-            {/* Mobile Drawer */}
             {mobileDrawerOpen && (
                 <div className='fixed top-0 left-1/2 transform -translate-x-1/2 z-[100] bg-black/90 w-full flex justify-center'>
                     <div className='bg-neutral-900 w-4/5 max-w-md p-6 flex flex-col justify-start items-center shadow-2xl rounded-lg mt-4'>
